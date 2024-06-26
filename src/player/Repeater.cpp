0f10cfa7cbf094a4efafa144f00fce65a4ed88a7
Noé Lopez <noelopez@free.fr>
2024-06-26 15:59:25 +0200

    import from zappy repo

---
+++ src/player/Repeater.cpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** Repeater.cpp
+*/
+
+#include "player/Repeater.hpp"
+
+namespace ia {
+    void Repeater::run(Player &player)
+    {
+        while (player.getInventory()[world::food] > 5) {
+            player.inventory();
+            while (!player.getUndesiredMessages().empty()) {
+                std::string data = player.getUndesiredMessages().at(0).getData();
+                player.broadcast(data);
+                player.eraseUndesiredMessage(0);
+            }
+        }
+        player.forkPlayer(RoleEnum::REAPEATER);
+        player.nbrConnect();
+        do {
+            player.nbrConnect();
+            usleep(100'000);
+        } while (player.getNbLeft() != 1);
+        player.startOtherPlayer(RoleEnum::REAPEATER);
+    }
+}
